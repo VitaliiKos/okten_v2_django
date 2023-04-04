fdf71c3fcdbf1931a5060638ce9e4116ce4e48f0
VitaliiKosyk <kostamtik@gmail.com>
2023-04-04 14:48:01 +0300

    django lesson 6

--- configs/settings.py
+++ configs/settings.py
@@ -18,10 +18,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l*1@qfeb^uokr*pql1o^)3d^s+8m+kt*5y+3su8-09@a=)70tm'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG") == 'True'
 
 ALLOWED_HOSTS = []
 
@@ -33,10 +33,11 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.staticfiles',
     'rest_framework_simplejwt',
+    'rest_framework_simplejwt.token_blacklist',
 
     # my_apps
     'core',
-    # 'apps.auth',
+    'apps.auth',
     'apps.users',
     'apps.cars',
     'apps.auto_parks',
@@ -73,11 +74,11 @@ WSGI_APPLICATION = 'configs.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'tamtik_django',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
-        'PORT': '3306'
+        'NAME': os.environ.get('MYSQL_DATABASE'),
+        'USER': os.environ.get('MYSQL_USER'),
+        'PASSWORD': os.environ.get('MYSQL_PASSWORD'),
+        'HOST': os.environ.get('MYSQL_HOST'),
+        'PORT': os.environ.get('MYSQL_PORT'),
     }}
 
 # Password validation
